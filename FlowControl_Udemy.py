40a297233a7f0361e231497a6fbd807da6ad8761
Devaraj Nayak <123163162+DevarajNayak18@users.noreply.github.com>
2023-02-16 18:05:08 +0530

    Add files via upload

---
+++ FlowControl_Udemy.py
@@ -0,0 +1,30 @@
+
+'''#Boolean Data types=True or False
+Boolean operators= and or
+#comaprison operator= ==,!=,<=,>=,>,<,
+
+print(42==42)
+print(42>=50)
+print(43!=42)
+
+myAGE=26
+print(myAGE>42)
+
+
+print(40.0==40)
+
+#OR operators Truth Table
+
+
+print(True or True)#True
+print(True or False)#True
+print(False or True)#True
+print(False or False)#False
+
+print(not False) #True
+print(not True)#False
+
+
+'''
+
+
